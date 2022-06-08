7f8e277d7b9572803cf86f6193fe7c99623da7d0
Luco97 <renatoluco@gmail.com>
2022-06-07 23:44:43 -0400

    ADD entidad de usuario

---
+++ src/database/models/user/user.entity.ts
@@ -0,0 +1,62 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'user',
+})
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'id',
+  })
+  uuid: string;
+
+  @Column({
+    name: 'image_src',
+    type: 'varchar',
+  })
+  image_src: string;
+
+  @Column({
+    name: 'password',
+    type: 'varchar',
+    nullable: false,
+    select: false,
+  })
+  password: string;
+
+  @Column({
+    name: 'email',
+    type: 'varchar',
+    nullable: false,
+  })
+  email: string;
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    name: 'last_name',
+    type: 'varchar',
+  })
+  last_name: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+  })
+  created_at: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    select: false,
+  })
+  deleted_at: Date;
+}
