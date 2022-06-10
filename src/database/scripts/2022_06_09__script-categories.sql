78e743ac790613ce57b9a35d8d97e47c17d3b5b2
Luco97 <renatoluco@gmail.com>
2022-06-10 18:23:23 -0400

    ADD sccript para crear categorias

---
+++ src/database/scripts/2022_06_09__script-categories.sql
@@ -0,0 +1,34 @@
+INSERT INTO
+    ecomm.category (id, name)
+VALUES
+    (1, 'Deporte');
+
+INSERT INTO
+    ecomm.category (id, name)
+VALUES
+    (2, 'Tecnologia');
+
+INSERT INTO
+    ecomm.category (id, name)
+VALUES
+    (3, 'Literatura');
+
+INSERT INTO
+    ecomm.category (id, name)
+VALUES
+    (4, 'Muebles y Accesorios');
+
+INSERT INTO
+    ecomm.category (id, name)
+VALUES
+    (5, 'Ropa');
+
+INSERT INTO
+    ecomm.category (id, name)
+VALUES
+    (6, 'Supermercado');
+
+INSERT INTO
+    ecomm.category (id, name)
+VALUES
+    (7, 'Juegos');
