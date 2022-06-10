4d5aaab72ae3d3b3191a5b91758e71df01d8fb72
Luco97 <renatoluco@gmail.com>
2022-06-09 23:37:49 -0400

    ADD se crea modulo principal y se importa base de datos

---
+++ src/ecomm/ecomm.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '../database/database.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), DatabaseModule],
+})
+export class EcommModule {}
