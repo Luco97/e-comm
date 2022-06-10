91d5e78dc782c699fd3963f671e635e74a29a822
Luco97 <renatoluco@gmail.com>
2022-06-10 18:23:42 -0400

    ADD script para agregar productos en base a script de categorias

---
+++ src/database/scripts/2022-06-10_script-products.sql
@@ -0,0 +1,1072 @@
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        1,
+        '',
+        0.4,
+        15,
+        519990.0,
+        '128GB CLOUD MINT',
+        'SAMSUNG GALAXY S20',
+        '2022-06-09 23:59:07.515',
+        '2022-06-09 23:59:07.515',
+        NULL,
+        2
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        7,
+        '',
+        0.25,
+        20,
+        150000.0,
+        'Marca china, pero de la buena',
+        'XIAOMI REDMI NOTE 11 128GB GRAFITO',
+        '2022-06-10 18:02:01.126',
+        '2022-06-10 18:02:01.126',
+        NULL,
+        2
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        8,
+        '',
+        0.2,
+        5,
+        499990.0,
+        'La manzanita',
+        'IPHONE 11',
+        '2022-06-10 18:02:33.646',
+        '2022-06-10 18:02:33.646',
+        NULL,
+        2
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        9,
+        '',
+        0.2,
+        15,
+        14500.0,
+        'Juego de Capcom de caza de animales prehistoricos',
+        'Moster Hunter: World',
+        '2022-06-10 18:03:38.650',
+        '2022-06-10 18:03:38.650',
+        NULL,
+        7
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        10,
+        '',
+        0.5,
+        3,
+        749000.0,
+        'INTEL® CORE I5 / 8GB RAM / 256GB SSD / NVIDIA GEFORCE RTX 3050 / 16.1'' FHD',
+        'HP VICTUS LAPTOP 16-D0500LA',
+        '2022-06-10 18:05:27.210',
+        '2022-06-10 18:05:27.210',
+        NULL,
+        2
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        11,
+        '',
+        0.0,
+        1,
+        550000.0,
+        'La mejor consola hecha por la humanidad',
+        'Sony Playstation 5',
+        '2022-06-10 18:05:27.210',
+        '2022-06-10 18:05:27.210',
+        NULL,
+        2
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        15,
+        '',
+        0.0,
+        10,
+        25000.0,
+        'Libro de JRR Tolkien',
+        'Lord of the Rings: Two Towers',
+        '2022-06-10 18:06:06.402',
+        '2022-06-10 18:06:06.402',
+        NULL,
+        3
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        16,
+        '',
+        0.6,
+        100,
+        15000.0,
+        'Una mierda de libro, Por favor no comprar',
+        'Papelucho',
+        '2022-06-10 18:06:06.402',
+        '2022-06-10 18:06:06.402',
+        NULL,
+        3
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        17,
+        '',
+        0.15,
+        5,
+        90990.0,
+        'Toda la colleccion de uno de los mejores mangakas de terror',
+        'Junji Ito: Collection',
+        '2022-06-10 18:06:21.835',
+        '2022-06-10 18:06:21.835',
+        NULL,
+        3
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        20,
+        '',
+        0.15,
+        5,
+        90990.0,
+        'Toda la colleccion de uno de los mejores mangakas de terror',
+        'Junji Ito: Collection',
+        '2022-06-10 18:07:08.019',
+        '2022-06-10 18:07:08.019',
+        NULL,
+        3
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        21,
+        '',
+        0.5,
+        27,
+        45000.0,
+        'El mejor manga de la histora CTM',
+        'Berserk',
+        '2022-06-10 18:07:08.019',
+        '2022-06-10 18:07:08.019',
+        NULL,
+        3
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        22,
+        '',
+        0.9,
+        10000,
+        1500.0,
+        'Deprorable desempeño, mejor que no vuelva',
+        'Hunter X Hunter: Capitulo 391',
+        '2022-06-10 18:07:24.644',
+        '2022-06-10 18:07:24.644',
+        NULL,
+        3
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        23,
+        '',
+        0.8,
+        12,
+        55000.0,
+        'Madera de calidad',
+        'Escritorio oficina',
+        '2022-06-10 18:07:40.492',
+        '2022-06-10 18:07:40.492',
+        NULL,
+        4
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        24,
+        '',
+        0.98,
+        50,
+        12000.0,
+        'Ilumina mucho',
+        'Lampara de escritorio',
+        '2022-06-10 18:07:40.492',
+        '2022-06-10 18:07:40.492',
+        NULL,
+        4
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        25,
+        '',
+        0.2,
+        20,
+        150000.0,
+        'Sillon de Tela persa',
+        'Sillon de visitas exportado',
+        '2022-06-10 18:07:40.492',
+        '2022-06-10 18:07:40.492',
+        NULL,
+        4
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        26,
+        '',
+        0.4,
+        21,
+        250000.0,
+        'Cama para no dejarla sola',
+        'Cama 2 plazas',
+        '2022-06-10 18:07:40.492',
+        '2022-06-10 18:07:40.492',
+        NULL,
+        4
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        27,
+        '',
+        0.5,
+        47,
+        77777.0,
+        'Para abrazar en las noches heladas',
+        'Almuada waifu',
+        '2022-06-10 18:07:40.492',
+        '2022-06-10 18:07:40.492',
+        NULL,
+        4
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        28,
+        '',
+        0.4,
+        25,
+        18990.0,
+        'Para los wuatones del hogar',
+        'Polera 3XL',
+        '2022-06-10 18:07:51.757',
+        '2022-06-10 18:07:51.757',
+        NULL,
+        5
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        29,
+        '',
+        0.2,
+        580,
+        28990.0,
+        'Para las reuniones en zoom',
+        'Chanclas de cuero de rinoceronte',
+        '2022-06-10 18:07:51.757',
+        '2022-06-10 18:07:51.757',
+        NULL,
+        5
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        30,
+        '',
+        0.9,
+        990,
+        10995.0,
+        'Otra vez, deprorable desempeño',
+        'Polera del anime mas sobrevalorado... HxH',
+        '2022-06-10 18:07:51.757',
+        '2022-06-10 18:07:51.757',
+        NULL,
+        5
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        31,
+        '',
+        0.15,
+        12,
+        27990.0,
+        'Para agarrarce a balazos con los panas',
+        'Falda del viejo de corazon valiente',
+        '2022-06-10 18:07:51.757',
+        '2022-06-10 18:07:51.757',
+        NULL,
+        5
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        32,
+        '',
+        0.77,
+        3,
+        35990.0,
+        'Para que no te cogoteen cuando vas a pistear como los dioses a la U',
+        'Poleron Anti Cogoteo',
+        '2022-06-10 18:07:51.757',
+        '2022-06-10 18:07:51.757',
+        NULL,
+        5
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        33,
+        '',
+        0.0,
+        150,
+        22222.0,
+        'Pelota de chilito ctm',
+        'Pelota de futbol',
+        '2022-06-10 18:08:05.174',
+        '2022-06-10 18:08:05.174',
+        NULL,
+        1
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        34,
+        '',
+        0.66,
+        666,
+        66666.0,
+        'instrumento que se pitio a un chino en los caminantes muertos',
+        'Bate de Baseball',
+        '2022-06-10 18:08:05.174',
+        '2022-06-10 18:08:05.174',
+        NULL,
+        1
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        35,
+        '',
+        0.8,
+        25,
+        21990.0,
+        'Para que te veas como subnormal y ver bajo el agua',
+        'Esnorquel ',
+        '2022-06-10 18:08:05.174',
+        '2022-06-10 18:08:05.174',
+        NULL,
+        1
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        36,
+        '',
+        0.4,
+        33,
+        33990.0,
+        'Polera del bulla campion',
+        'Polera de Universidad de Chile',
+        '2022-06-10 18:08:05.174',
+        '2022-06-10 18:08:05.174',
+        NULL,
+        1
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        37,
+        '',
+        0.1,
+        225,
+        37090.0,
+        'Solo para cuicos',
+        'Palo de golf',
+        '2022-06-10 18:08:05.174',
+        '2022-06-10 18:08:05.174',
+        NULL,
+        1
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        38,
+        '',
+        0.8,
+        101,
+        1990.0,
+        'Mani pal mono',
+        'Mani',
+        '2022-06-10 18:08:30.245',
+        '2022-06-10 18:08:30.245',
+        NULL,
+        6
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        39,
+        '',
+        0.0,
+        15,
+        2500.0,
+        'Dale verde a tu cuerpo',
+        'Lechuga',
+        '2022-06-10 18:08:30.245',
+        '2022-06-10 18:08:30.245',
+        NULL,
+        6
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        40,
+        '',
+        0.0,
+        97,
+        14700.0,
+        'Para que lo haga el wuaton parrillero',
+        'Costillar con merquen',
+        '2022-06-10 18:08:30.245',
+        '2022-06-10 18:08:30.245',
+        NULL,
+        6
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        41,
+        '',
+        0.1,
+        64,
+        11990.0,
+        'Para las golozas de la casa',
+        'Pack de 9 Longanizas del abuelo',
+        '2022-06-10 18:08:30.245',
+        '2022-06-10 18:08:30.245',
+        NULL,
+        6
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        42,
+        '',
+        0.15,
+        47,
+        12490.0,
+        'Lo que nunca falta',
+        'Papas fritas Marco Polo',
+        '2022-06-10 18:08:30.245',
+        '2022-06-10 18:08:30.245',
+        NULL,
+        6
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        43,
+        '',
+        0.2,
+        15,
+        14500.0,
+        'Juego de Capcom de caza de animales prehistoricos',
+        'Moster Hunter: World',
+        '2022-06-10 18:08:41.758',
+        '2022-06-10 18:08:41.758',
+        NULL,
+        7
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        44,
+        '',
+        0.3,
+        25,
+        4864.0,
+        'Uno de los juegos de la mejor la saga',
+        'The Curse of the Mokey Island',
+        '2022-06-10 18:08:41.758',
+        '2022-06-10 18:08:41.758',
+        NULL,
+        7
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        45,
+        '',
+        0.99,
+        999,
+        1500.0,
+        'Juego de mierda malo, es mejor pirateado',
+        'Ciberpunk 2077',
+        '2022-06-10 18:08:41.758',
+        '2022-06-10 18:08:41.758',
+        NULL,
+        7
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        46,
+        '',
+        0.25,
+        13,
+        45000.0,
+        'Juego SOLO para Giga Chad',
+        'BloodBorne',
+        '2022-06-10 18:08:41.758',
+        '2022-06-10 18:08:41.758',
+        NULL,
+        7
+    );
+
+INSERT INTO
+    ecomm.product (
+        id,
+        image_src,
+        offer,
+        stock,
+        price,
+        description,
+        name,
+        created_at,
+        updated_at,
+        deleted_at,
+        categoryId
+    )
+VALUES
+    (
+        47,
+        '',
+        0.64,
+        12,
+        55000.0,
+        'Lo mejor de la simulacion de carreras',
+        'Gran Turismo 7',
+        '2022-06-10 18:08:41.758',
+        '2022-06-10 18:08:41.758',
+        NULL,
+        7
+    );
