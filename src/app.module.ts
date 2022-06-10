b8570339fca4d5d297b652b658cbbecda91b5aa1
Luco97 <renatoluco@gmail.com>
2022-06-09 23:38:11 -0400

    UPDATE se deja modulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { EcommModule } from './ecomm/ecomm.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [EcommModule],
   controllers: [AppController],
   providers: [AppService],
 })
