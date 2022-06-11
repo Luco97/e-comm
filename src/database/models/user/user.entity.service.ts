ccd037d35e84518a4b5c9367e8ba27821f1edfe5
Luco97 <renatoluco@gmail.com>
2022-06-10 21:12:17 -0400

    UPDATE se injecta repository para usuarios

--- src/database/models/user/user.entity.service.ts
+++ src/database/models/user/user.entity.service.ts
@@ -1,4 +1,12 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
 
 @Injectable()
-export class UserEntityService {}
+export class UserEntityService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private _userRepo: Repository<UserEntity>,
+  ) {}
+}
