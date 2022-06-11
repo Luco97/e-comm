3a731fdb2318404f3addb294f78c23d155959c37
Luco97 <renatoluco@gmail.com>
2022-06-10 21:11:49 -0400

    UPDATE se injecta repository para ordenes

--- src/database/models/order/order.entity.service.ts
+++ src/database/models/order/order.entity.service.ts
@@ -1,4 +1,12 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OrderEntity } from './order.entity';
 
 @Injectable()
-export class OrderEntityService {}
+export class OrderEntityService {
+  constructor(
+    @InjectRepository(OrderEntity)
+    private _orderRepo: Repository<OrderEntity>,
+  ) {}
+}
