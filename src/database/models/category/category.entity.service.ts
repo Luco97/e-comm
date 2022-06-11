498e3139d510572a954cfe2c95147503eca7f7c9
Luco97 <renatoluco@gmail.com>
2022-06-10 21:11:39 -0400

    UPDATE se injecta repository para categorias

--- src/database/models/category/category.entity.service.ts
+++ src/database/models/category/category.entity.service.ts
@@ -1,6 +1,12 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from './category.entity';
 
 @Injectable()
 export class CategoryEntityService {
-    
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private _categoryRepo: Repository<CategoryEntity>,
+  ) {}
 }
