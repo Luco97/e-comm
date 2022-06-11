45d9b8a2e0feb5d31df6d36f207b8436a9d806ce
Luco97 <renatoluco@gmail.com>
2022-06-10 21:11:55 -0400

    UPDATE se injecta repository para productos

--- src/database/models/product/product.entity.service.ts
+++ src/database/models/product/product.entity.service.ts
@@ -1,4 +1,12 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductEntity } from './product.entity';
 
 @Injectable()
-export class ProductEntityService {}
+export class ProductEntityService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private _productRepo: Repository<ProductEntity>,
+  ) {}
+}
