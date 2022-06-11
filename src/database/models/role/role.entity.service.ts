ec839df08f845e8df43e123bf4b0888b76b38a51
Luco97 <renatoluco@gmail.com>
2022-06-10 21:12:02 -0400

    UPDATE se injecta repository para roles

--- src/database/models/role/role.entity.service.ts
+++ src/database/models/role/role.entity.service.ts
@@ -1,4 +1,12 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RoleEntity } from './role.entity';
 
 @Injectable()
-export class RoleEntityService {}
+export class RoleEntityService {
+  constructor(
+    @InjectRepository(RoleEntity)
+    private _roleRepo: Repository<RoleEntity>,
+  ) {}
+}
