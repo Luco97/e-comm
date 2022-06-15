be6eee14ae223d9dc6d3bbc8fada69096b9b4dc7
Luco97 <renatoluco@gmail.com>
2022-06-15 02:16:03 -0400

    UPDATE endpoints para creacion, login y update de un usuario

--- src/ecomm/controller/auth.controller.ts
+++ src/ecomm/controller/auth.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Login, Sign, Update } from '@ecomm/dtos/user';
+import {
+  Body,
+  Controller,
+  Headers,
+  Post,
+  Put,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { tap, from } from 'rxjs';
+import { AuthGuard } from '../guards/auth.guard';
+import { response } from '../interfaces/response';
+import { UserService } from '../services/user.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private _userService: UserService) {}
+  @Post('sign')
+  signIn(@Body() signBody: Sign, @Res() resp: Response<response>) {
+    from(this._userService.register(signBody))
+      .pipe(tap((data) => resp.status(data.status).json(data)))
+      .subscribe();
+  }
+
+  @Post('login')
+  logIn(@Body() loginBody: Login, @Res() resp: Response<response>) {
+    from(this._userService.login(loginBody))
+      .pipe(tap((data) => resp.status(data.status).json(data)))
+      .subscribe();
+  }
+
+  @Put('update')
+  @UseGuards(AuthGuard)
+  updateFields(
+    @Headers('authorization') auth: string,
+    @Body() updateBody: Update,
+    @Res() resp: Response<response>,
+  ) {
+    const token: string = auth.replace(/Bearer /g, '');
+    from(this._userService.updateInfo(updateBody, token))
+      .pipe(tap((data) => resp.status(data.status).json(data)))
+      .subscribe();
+  }
+}
