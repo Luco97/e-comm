dd7846acd2bdc3dc1fb51ac5f60f0f7d29a441fa
Luco97 <renatoluco@gmail.com>
2022-06-15 02:13:59 -0400

    UPDATE crud basico para entidad de usuario

--- src/database/models/user/user.entity.service.ts
+++ src/database/models/user/user.entity.service.ts
@@ -1,4 +1,4 @@
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
@@ -9,4 +9,24 @@ export class UserEntityService {
     @InjectRepository(UserEntity)
     private _userRepo: Repository<UserEntity>,
   ) {}
+  create(newUser: DeepPartial<UserEntity>): Promise<UserEntity> {
+    const user = this._userRepo.create(newUser);
+    return this._userRepo.save(user);
+  }
+  findOne(uuid: string): Promise<UserEntity> {
+    return this._userRepo
+      .createQueryBuilder('user')
+      .where('user.uuid = :uuid', { uuid })
+      .getOne();
+  }
+  update(user: UserEntity): Promise<UserEntity> {
+    const updateUser = this._userRepo.create(user);
+    return this._userRepo.save(updateUser);
+  }
+  findMail(email: string): Promise<UserEntity> {
+    return this._userRepo
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email })
+      .getOne();
+  }
 }
