7410581b828e4f9b56a41b26d6bf04b130b4feef
Luco97 <renatoluco@gmail.com>
2022-06-18 00:51:13 -0400

    UPDATE prefijo para todos los controladores excepto app y se agrega puerto

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api', {
+    exclude: [{ method: RequestMethod.GET, path: '' }],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
