73ed4f51bb4f9b5dd4f9d4da2fa3341ced998c76
Luco97 <renatoluco@gmail.com>
2022-06-19 17:37:20 -0400

    UPDATE se crea metodo para obtener todas las ordenes por un usuario

--- src/ecomm/services/order.service.ts
+++ src/ecomm/services/order.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { OrderEntityService } from '@database/models/order';
+import { Injectable, HttpStatus } from '@nestjs/common';
+import { UtilsService } from '@shared/auth';
+import { response } from '../interfaces/response';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(
+    private _utilsService: UtilsService,
+    private _orderEntityService: OrderEntityService,
+  ) {}
+
+  async getAllOrders(
+    parameters: {
+      orderBy: string;
+      order: 'ASC' | 'DESC';
+      take: any;
+      skip: any;
+    },
+    token: string,
+  ): Promise<response> {
+    const user_uuid = this._utilsService.userUuid(token);
+    const [orders, count] = await this._orderEntityService.findAll(
+      parameters,
+      user_uuid,
+    );
+    return {
+      status: HttpStatus.OK,
+      message: `orders of => ${user_uuid}`,
+      response: {
+        orders,
+        count,
+      },
+    };
+  }
+}
