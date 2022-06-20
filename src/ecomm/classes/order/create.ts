c2c7cd87ea08e926a40f856c96109e12a5f2e6d2
Luco97 <renatoluco@gmail.com>
2022-06-20 19:44:32 -0400

    UPDATE se agrega direccion para dto de creacion para orden

--- src/ecomm/classes/order/create.ts
+++ src/ecomm/classes/order/create.ts
@@ -1,6 +1,15 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class Create {
   @IsNumber({}, { each: true })
   products: number[];
+
+  @IsString()
+  city: string;
+
+  @IsString()
+  address: string;
+
+  @IsString()
+  council: string;
 }
