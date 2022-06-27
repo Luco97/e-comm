a8063f6e85f29d76ca36730e0b420596e4e54746
Luco97 <renatoluco@gmail.com>
2022-06-27 16:30:09 -0400

    ADD dto para cambiar info de producto

---
+++ src/ecomm/classes/product/update.ts
@@ -0,0 +1,29 @@
+import { IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
+
+export class Update {
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsOptional()
+  @IsString()
+  image_src: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Max(1)
+  offer: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  stock: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  price: number;
+}
