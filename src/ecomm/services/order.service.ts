eb8c72e5357dc9cbddaeec5a6ab96a928612c34c
Luco97 <renatoluco@gmail.com>
2022-06-28 23:08:07 -0400

    UPDATE se quita parametro extra

--- src/ecomm/services/order.service.ts
+++ src/ecomm/services/order.service.ts
@@ -64,7 +64,7 @@ export class OrderService {
         from(
           this._productEntityService.findOne(
             product.product_id,
-            product.quantity,
+            // ,product.quantity,
           ),
         ),
       );
