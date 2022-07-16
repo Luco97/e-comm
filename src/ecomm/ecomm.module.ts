dfa96e1922d4a4abea390197ccd7fffb9a128bd9
Luco97 <renatoluco@gmail.com>
2022-07-16 17:17:41 -0400

    UPDATE import path y orden de imports

--- src/ecomm/ecomm.module.ts
+++ src/ecomm/ecomm.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
+// Modules
+import { AuthModule } from '@shared/auth/auth.module';
+import { RolesModule } from '@shared/roles/roles.module';
 import { DatabaseModule } from '../database/database.module';
+
+// Controllers
 import { AuthController } from './controller/auth.controller';
 import { OrderController } from './controller/order.controller';
 import { ProductController } from './controller/product.controller';
 import { CategoryController } from './controller/category.controller';
+
+// Services
 import { UserService } from './services/user.service';
 import { OrderService } from './services/order.service';
 import { ProductService } from './services/product.service';
-import { AuthModule } from '../shared/auth/auth.module';
-import { RolesModule } from '@shared/roles/roles.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, AuthModule, RolesModule],
