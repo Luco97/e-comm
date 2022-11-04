1ca33cae4549a388a532bb03df1709d4ef109478
Luco97 <renatoluco@gmail.com>
2022-11-04 00:18:14 -0300

    ADD tipados para metodos en service de entidad role

--- src/database/models/role/role.entity.service.ts
+++ src/database/models/role/role.entity.service.ts
@@ -10,14 +10,15 @@ export class RoleEntityService {
     @InjectRepository(RoleEntity)
     private _roleRepo: Repository<RoleEntity>,
   ) {}
-  findRole(type: string) {
+
+  findRole(type: string): Promise<RoleEntity> {
     return this._roleRepo
       .createQueryBuilder('role')
       .where('role.type = :type', { type })
       .getOne();
   }
 
-  findByRole(type: string, uuid: string) {
+  findByRole(type: string, uuid: string): Promise<number> {
     return this._roleRepo
       .createQueryBuilder('role')
       .leftJoin('role.user', 'user', 'user.uuid = :uuid', { uuid })
