57469c8caa07921091b29355b6dffad995ee74ee
Luco97 <renatoluco@gmail.com>
2022-12-02 20:26:41 -0300

    CREATE entidad para guardad variedad que modifica el producto

---
+++ src/database/models/extra/extra.entity.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { ProductEntity } from '../product/product.entity';
+
+@Entity()
+export class ExtraEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', length: '20' })
+  key: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  image_src: string;
+
+  @Column({ type: 'real', default: 1 })
+  price_variation: number;
+
+  @Column({ type: 'int', default: 0 })
+  stock: number;
+
+  @ManyToOne(() => ProductEntity, (product) => product.extras)
+  product: ProductEntity;
+}
