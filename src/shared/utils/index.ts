e86d8945caadc860eb6caec1a4360fa9af43492e
Luco97 <renatoluco@gmail.com>
2022-12-02 18:25:32 -0300

    DELETE se quita lesera que no estoy ya no voy a utilizar

--- src/shared/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { dateType } from './types.interface';
--- src/shared/utils/types.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface dateType {
-    day: number;
-    month: number;
-    year: number;
-    hour: number;
-    minute: number;
-    second: number;
-  }
