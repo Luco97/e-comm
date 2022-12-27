db13cdf1e93516b40be58750a7feafcd3351cf4d
Luco97 <renatoluco@gmail.com>
2022-12-27 20:44:24 -0300

    UPDATE se cambia nombre de entidad en DB

--- src/database/models/extra/extra.entity.ts
+++ src/database/models/extra/extra.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { ProductEntity } from '../product/product.entity';
 
-@Entity()
+@Entity({ name: 'variation' })
 export class ExtraEntity {
   @PrimaryGeneratedColumn('increment')
   id: number;
