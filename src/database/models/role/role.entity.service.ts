4b62d88c348270faa3b1a1e437b9bad038c4b374
Luco97 <renatoluco@gmail.com>
2025-06-01 22:20:51 -0400

    DELETE se quita codigo sin utilizar (try catch)

--- src/database/models/role/role.entity.service.ts
+++ src/database/models/role/role.entity.service.ts
@@ -2,7 +2,6 @@ import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RoleEntity } from './role.entity';
-import { from, map } from 'rxjs';
 
 @Injectable()
 export class RoleEntityService {
@@ -25,9 +24,5 @@ export class RoleEntityService {
       .where('role.type = :type', { type })
       .andWhere('user.uuid = :uuid', { uuid })
       .getCount();
-    try {
-    } catch (error) {
-      return new Promise<number>((resolve) => 0);
-    }
   }
 }
